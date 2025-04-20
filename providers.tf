8d64af476019059bf1f4f9f0e08f370f0befd6db
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-04-20 10:41:17 -0300

    change local state, local to cloud

--- providers.tf
+++ providers.tf
@@ -5,8 +5,23 @@ terraform {
       version = "5.95.0"
     }
   }
+  backend "s3" {
+    bucket  = "rocketseat-bielb1b2-state-bucket"
+    region  = "us-east-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+
+    profile = "AdministratorAccess-001348462846"
+  }
 }
 
 provider "aws" {
   profile = "AdministratorAccess-001348462846"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+  lifecycle {
+    prevent_destroy = true
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type        = string
+  default     = "rocketseat-bielb1b2-state-bucket"
+  description = "State bucket of TF"
+}
